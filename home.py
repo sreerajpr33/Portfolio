350e3e3df6e29341ac7389f41c19deb324e9eeea
sreerajpr33 <prsreeraj33@gmail.com>
2025-01-09 16:55:02 +0530

    main_project

--- home.py
+++ home.py
@@ -1,5 +1,12 @@
 from flask import Flask,render_template
+import sqlite3
 app=Flask(__name__)
+
+con=sqlite3.connect("data.db")
+try:
+    con.execute("create table contact(name text,email text,subject text,message text)")
+except: 
+    pass
 @app.route('/')
 def index():
     return render_template("index.html")    
